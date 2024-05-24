27ec0d0ed7e2cf728d73ff3debbb69d2f46f2786
Rebecca Millington <cobalt93@googlemail.com>
2024-05-24 12:03:56 +0100

    adding kmeans model option

--- ModelViz.py
+++ ModelViz.py
@@ -169,9 +169,9 @@ class ModelViz:
         self.tsds = pd.read_csv(file_path)
         self.index = self.tsds.index
 
-    def train(self, tsds=None, n_clusters=6, method='quantile', verbose=True, save=True, file_path='model.ks'):
+    def train(self, tsds=None, n_clusters=6, method='quantile', verbose=True, save=True, file_path='model.ks', model_name='kshape'):
         """
-        Train the clustering model using KShape algorithm.
+        Train the clustering model using either KShape (for time series data) or KMeans (for single time point data).
 
         Args:
             tsds (pd.DataFrame): Time series dataset.
@@ -187,25 +187,27 @@ class ModelViz:
         if tsds is None:
             tsds = self.tsds
         self.n_clusters = n_clusters
-        if method == 'quantile':
-            print('Initialising using quantiles')
-            quantiles = np.arange(1 / (2 * n_clusters), 1, 1 / n_clusters)
-            self.model = ts.clustering.KShape(n_clusters=n_clusters,
-                                              verbose=verbose,
-                                              init=tsds.quantile(q=quantiles).values[:, :, np.newaxis],
-                                              n_init=1
-                                              )
-        elif method == 'random':
-            print('Initialising using random, seed = ', self.seed)
-            self.model = ts.clustering.KShape(n_clusters=n_clusters,
-                                              verbose=verbose,
-                                              random_state=self.seed,
-                                              n_init=self.n_init
-                                              )
-        else:
-            print('Unrecognised initialisation method')
-            return
-
+        if model_name == 'kmeans':
+            self.model = KMeans(init="k-means++", n_clusters=n_clusters, n_init=self.n_init, random_state=self.seed)
+        if model_name == 'kshape':
+            if method == 'quantile':
+                print('Initialising using quantiles')
+                quantiles = np.arange(1 / (2 * n_clusters), 1,1 / n_clusters)
+                self.model = ts.clustering.KShape(n_clusters=n_clusters,
+                                           verbose=verbose,
+                                           init=tsds.quantile(q=quantiles).values[:, :, np.newaxis],
+                                           n_init = 1
+                                           )
+            elif method == 'random':
+                print('Initialising using random, seed = ', self.seed)
+                self.model = ts.clustering.KShape(n_clusters=n_clusters,
+                                   verbose=verbose,
+                                   random_state=self.seed,
+                                   n_init = self.n_init
+                                   )
+            else:
+                print('Unrecognised initialisation method')
+                return
         self.model.fit(tsds)
         if save:
             self.model.to_json(file_path)
